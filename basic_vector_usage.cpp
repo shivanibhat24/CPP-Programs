2c116dabb801f49266267984661d9cee2f5af98e
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-01-29 10:11:24 +0530

    Create basic_vector_usage.cpp

---
+++ basic_vector_usage.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<char> a={'a','b','c','d','e'};
+    for(char val:a){
+        cout<<val<<endl;
+    }
+    vector<int> vect={1,2,3,4,5};
+    for (int i: vect)
+    cout<<i<<endl;
+    return 0;
+}
