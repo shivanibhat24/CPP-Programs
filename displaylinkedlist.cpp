e154c9cd6cbcf98b07c612b3a02a6232ae7f2725
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-02-02 19:31:43 +0530

    Add files via upload

---
+++ displaylinkedlist.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+// Node structure
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+// Function to insert a node at the end of the linked list
+void insert(Node*& head, int val) {
+    Node* newNode = new Node(val);
+    if (!head) {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while (temp->next) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+// Function to display the linked list
+void display(Node* head) {
+    if (!head) {
+        cout << "Linked List is empty!" << endl;
+        return;
+    }
+    Node* temp = head;
+    cout << "Linked List: ";
+    while (temp) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    Node* head = nullptr;
+    int n, val;
+
+    cout << "Enter the number of nodes: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter value for node " << i + 1 << ": ";
+        cin >> val;
+        insert(head, val);
+    }
+
+    // Display the linked list
+    display(head);
+
+    return 0;
+}
