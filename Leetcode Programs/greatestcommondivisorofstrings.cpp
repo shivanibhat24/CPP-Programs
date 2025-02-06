2038c6ffdd6567001d9badbbab4a368ccec9b78b
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-02-06 10:25:20 +0530

    Create greatestcommondivisorofstrings.cpp

---
+++ Leetcode Programs/greatestcommondivisorofstrings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1) {
+            return ""; 
+        }
+        int gcd_len = gcd(str1.length(), str2.length());   
+        return str1.substr(0, gcd_len);
+    }
+};
