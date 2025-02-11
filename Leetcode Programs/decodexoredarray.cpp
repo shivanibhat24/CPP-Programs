db1e6024e9fdd9f57bf6da3ab110f2b071771200
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-02-11 10:05:57 +0530

    Create decodexoredarray.cpp

---
+++ Leetcode Programs/decodexoredarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        int n=encoded.size();
+        vector<int>arr(n+1);
+        arr[0] = first;
+        for(int i = 0; i < encoded.size(); i++){
+            arr[i+1] = arr[i]^encoded[i];
+        }
+        return arr;
+    }
+};
