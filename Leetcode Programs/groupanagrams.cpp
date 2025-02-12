d09b2a42e2c8ed2f9c158078a432b3997285a145
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-02-12 06:18:49 +0530

    Update groupanagrams.cpp

--- Leetcode Programs/groupanagrams.cpp
+++ Leetcode Programs/groupanagrams.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> anagrams;
-        for (const auto& str: strs)
-        {
-            string s(str);
-            sort(s.begin(), s.end());
-            anagrams[s].push_back(str);
+        vector<pair<int, int>> mapping;
+        for (int i = 0; i < strs.size(); ++i) {
+            uint hash = 0, counts[26] {0};
+            for (const auto& ch: strs[i]) ++counts[ch - 'a'];
+            for (int j = 0; j < 26; ++j) hash *= 31, hash ^= counts[j];
+            mapping.emplace_back(hash, i); 
         }
-        vector<vector<string>> result;
-        result.reserve(anagrams.size());
-        for (const auto& [key, arr]: anagrams)
-        {
-            result.push_back(arr);
+        ranges::sort(mapping);
+        vector<vector<string>> res;
+        res.push_back({move(strs[mapping[0].second])});
+        for (int i = 1; i < mapping.size(); ++i) {
+            if (mapping[i].first == mapping[i - 1].first)
+                res.back().emplace_back(move(strs[mapping[i].second]));
+            else
+                res.push_back({move(strs[mapping[i].second])});
         }
-        return result;
+        return res; 
     }
 };
