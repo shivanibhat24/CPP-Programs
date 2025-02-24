0ff2d346874e8038fe0ecb14f74bc2bc8679eb54
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-02-24 12:15:20 +0530

    Create permutationsequence.cpp

---
+++ Leetcode Programs/permutationsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int> nums;
+        int total = 1;
+        for (int i = 1; i <= n; ++i) {
+            nums.emplace_back(i);
+            total *= i;
+        }
+        int group = total;
+        stringstream permutation;
+        while (n > 0) {
+            group /= n;
+            int idx = (k - 1) / group;
+            permutation << nums[idx];
+            nums.erase(nums.begin() + idx);
+            k = (k - 1) % group + 1;
+            --n;
+        }
+        return permutation.str();
+    }
+};
