e9ff6141eb9c1da4ac8e61df4e0497b9d3fb60d5
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-03-02 20:08:09 +0530

    Create countingbits.cpp

---
+++ Leetcode Programs/countingbits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res;        
+        res.reserve(n + 1);
+        res.push_back(0);
+        for (int i = 1; i <= n; ++i) {
+
+                res.push_back(res[ i / 2] + (i & 1));
+        }
+        return res;
+    }
+};
