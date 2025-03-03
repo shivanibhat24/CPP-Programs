4a5e1a9bf5074740916cad7bd9a3a488d468b134
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-03-03 06:45:08 +0530

    Update climbingstairs.cpp

--- Leetcode Programs/climbingstairs.cpp
+++ Leetcode Programs/climbingstairs.cpp
@@ -4,6 +4,12 @@ public:
         if (n == 0 || n == 1) {
             return 1;
         }
-        return climbStairs(n-1) + climbStairs(n-2);
+        int prev = 1, curr = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+        return curr;
     }
 };
