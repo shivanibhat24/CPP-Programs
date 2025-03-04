8f4686d8d30266206820c6c7400e6271ad2ad093
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-03-04 09:51:43 +0530

    Create minimumpathsum.cpp

---
+++ Leetcode Programs/minimumpathsum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long solve(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp){
+        if(i==0 && j==0) return grid[0][0]; // destination reached
+        if(i<0 || j<0) return INT_MAX;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        long long up = grid[i][j] + solve(i-1, j, grid, dp);
+        long long left = grid[i][j] + solve(i, j-1, grid, dp);
+
+        return dp[i][j] = min(up, left);
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        return solve(m-1, n-1, grid, dp);
+    }
+};
