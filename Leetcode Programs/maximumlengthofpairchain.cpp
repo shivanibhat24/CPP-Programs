120929528700069882bcd3fe88084e59c7e5edfe
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-03-05 09:14:15 +0530

    Create maximumlengthofpairchain.cpp

---
+++ Leetcode Programs/maximumlengthofpairchain.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    private:
+    static bool cmp(vector<int>&a, vector<int>&b){
+        return a[1]<b[1];
+    }
+public:
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(), pairs.end(), cmp);
+        int cnt = 1;
+        int prev = pairs[0][1];
+        for(int i = 1; i<pairs.size(); i++){
+        if(prev < pairs[i][0]){
+            cnt++;
+            prev = pairs[i][1];
+        }
+        }
+            return cnt;
+    }
+};
