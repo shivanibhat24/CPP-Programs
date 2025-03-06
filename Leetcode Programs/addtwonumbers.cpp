bd66ab9a2f32a1b09661ec9f416912838d13e21c
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-03-06 09:26:01 +0530

    Update addtwonumbers.cpp

--- Leetcode Programs/addtwonumbers.cpp
+++ Leetcode Programs/addtwonumbers.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummyHead = new ListNode(0);
-        ListNode* tail = dummyHead;
-        int carry = 0;
-
-        while (l1 != nullptr || l2 != nullptr || carry != 0) {
-            int digit1 = (l1 != nullptr) ? l1->val : 0;
-            int digit2 = (l2 != nullptr) ? l2->val : 0;
-
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode* newNode = new ListNode(digit);
-            tail->next = newNode;
-            tail = tail->next;
-
-            l1 = (l1 != nullptr) ? l1->next : nullptr;
-            l2 = (l2 != nullptr) ? l2->next : nullptr;
+        ListNode a(0);
+        ListNode* b = &a;
+        int c=0;
+        while(l1 != nullptr || l2 != nullptr || c>0)        
+        {
+        if(l1 != nullptr)
+        {
+            c += l1->val;
+            l1 = l1->next;
         }
+        if(l2 != nullptr)
+        {
+            c += l2->val;
+            l2 = l2->next;
+        }
+        b->next = new ListNode(c%10);
+        c /= 10;
+        b = b-> next;
+        }
+        return a.next;
 
-        ListNode* result = dummyHead->next;
-        delete dummyHead;
-        return result;
     }
 };
