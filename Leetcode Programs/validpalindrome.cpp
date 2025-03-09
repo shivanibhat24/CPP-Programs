cb79c526fff6c93d071510e520fdd9cb47d505a6
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-03-09 19:26:28 +0530

    Create validpalindrome.cpp

---
+++ Leetcode Programs/validpalindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string s2;
+        for (char c : s) {
+            if (isalnum(c)) {
+                s2.push_back(tolower(c));
+            }
+        }
+        int left = 0, right = s2.size() - 1;
+        while (left < right) {
+            if (s2[left] != s2[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
