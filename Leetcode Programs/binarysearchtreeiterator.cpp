b938b1bccb36241d2e2ff3043749cac3752f9203
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-03-12 09:53:23 +0530

    Create binarysearchtreeiterator.cpp

---
+++ Leetcode Programs/binarysearchtreeiterator.cpp
@@ -0,0 +1,20 @@
+class BSTIterator {
+private: stack<TreeNode *> myStack;
+public:
+    BSTIterator(TreeNode* root) {
+        pushAll(root);
+    }    
+    int next() {
+        TreeNode* tempNode = myStack.top();
+        myStack.pop();
+        pushAll(tempNode->right);
+        return tempNode->val;
+    }    
+    bool hasNext() {
+        return !myStack.empty();
+    }
+private:
+    void pushAll(TreeNode *node){
+        for(; node != NULL; myStack.push(node), node = node->left);
+    }
+};
