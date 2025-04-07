4facb99a40b1cb4922e7502dc2364450e36c6303
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-04-07 06:47:03 +0530

    Create partitionequalsubsetsum.cpp

---
+++ Leetcode Programs/partitionequalsubsetsum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int s = accumulate(nums.begin(), nums.end(), 0);
+        if(s%2) return false;
+        bitset<10001> bit(1);
+        for(auto i : nums){
+            bit |= bit<<i;
+            if(bit[s/2]) return bit[s/2];
+        }
+        return bit[s/2];
+    }
+};
