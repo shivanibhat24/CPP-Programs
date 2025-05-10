cfd623c461adbcf25c39b6a628a829b776a129ff
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-05-10 22:51:00 +0530

    Create minimumequalsumoftwoarraysafterreplacingzeroes.cpp

---
+++ Leetcode Programs/minimumequalsumoftwoarraysafterreplacingzeroes.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long minSum(vector<int>& nums1, vector<int>& nums2) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        long long sum1=0,sum2=0;
+        bool flag1=false,flag2=false;
+        for(auto &n :nums1){
+            sum1+=n;
+            if(n==0) {
+                sum1++;
+                flag1=true;
+            }
+        }
+        for(auto &n :nums2){
+            sum2+=n;
+            if(n==0) {
+                sum2++;
+                flag2=true;
+            }
+        }
+        if(flag1==false && sum1<sum2 || flag2==false && sum2<sum1){
+            return -1;
+        } 
+        return max(sum1,sum2);
+
+    }
+};
