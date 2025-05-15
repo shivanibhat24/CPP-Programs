173a6adc3057235b4bb2b248e0468f0b6705939f
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-05-15 09:40:58 +0530

    Create stonegame9.cpp

---
+++ Leetcode Programs/stonegame9.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int count[3] = {0};
+        for (int stone : stones) {
+            count[stone % 3]++;
+        }
+        if (count[0] % 2 == 0) {
+            return count[1]!= 0 && count[2]!= 0;
+        } else {
+            return abs(count[1] - count[2]) >= 3;
+        }
+    }
+};
