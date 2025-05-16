a4f9e3342f9b1c21872efeef37199b16964625ad
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-05-16 09:54:26 +0530

    Create firstuniquecharcaterinastring.cpp

---
+++ Leetcode Programs/firstuniquecharcaterinastring.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int frequency[26] = {0}; 
+        for (char c : s) {
+            frequency[c - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (frequency[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
