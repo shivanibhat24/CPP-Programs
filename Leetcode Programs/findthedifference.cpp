291339af04f6db983cbfb179f6f106e90212b06d
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-05-16 09:57:22 +0530

    Create findthedifference.cpp

---
+++ Leetcode Programs/findthedifference.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+    unordered_map<char,int>mpp;
+    for(auto ch:s) mpp[ch]++;
+    for(auto ch:t) mpp[ch]--;
+    for(auto it:mpp){
+        if(it.second<0) return it.first;
+    }
+    return ' ';
+    }
+};
