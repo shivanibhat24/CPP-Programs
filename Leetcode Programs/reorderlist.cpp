85e5a41a93ddb13606d9e0d5dc7880a33c8a48b5
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-05-20 09:14:39 +0530

    Create reorderlist.cpp

---
+++ Leetcode Programs/reorderlist.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (!head) return;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* second = slow->next;
+        slow->next = nullptr;
+        ListNode* node = nullptr;
+        while (second) {
+            ListNode* temp = second->next;
+            second->next = node;
+            node = second;
+            second = temp;
+        }
+        ListNode* first = head;
+        second = node;
+        while (second) {
+            ListNode* temp1 = first->next;
+            ListNode* temp2 = second->next;
+            first->next = second;
+            second->next = temp1;
+            first = temp1;
+            second = temp2;
+        }
+    }
+};
