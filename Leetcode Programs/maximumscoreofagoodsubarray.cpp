838262f79c6c15b4b5c920458660e2b2f9fe06b1
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-06-09 07:30:11 +0530

    Update maximumscoreofagoodsubarray.cpp

--- Leetcode Programs/maximumscoreofagoodsubarray.cpp
+++ Leetcode Programs/maximumscoreofagoodsubarray.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     int maximumScore(vector<int>& nums, int k) {
-        int n = nums.size();        
-        vector<int> leftBoundary(n, -1);
-        vector<int> rightBoundary(n, n); 
-        stack<int> leftStack;
-        stack<int> rightStack;
-        for (int i = n - 1; i >= 0; i--) {
-            while (!leftStack.empty() && nums[leftStack.top()] > nums[i]) {
-                leftBoundary[leftStack.top()] = i;
-                leftStack.pop();
-            }
-            leftStack.push(i);
+        int i = k,j=k;
+        int n  = nums.size();
+        int maxi = nums[k],mini = nums[k];
+        while( i>0 || j<n-1 ){
+        if(i==0){
+            j++;
         }
-        for (int i = 0; i < n; i++) {
-            while (!rightStack.empty() && nums[rightStack.top()] > nums[i]) {
-                rightBoundary[rightStack.top()] = i; 
-                rightStack.pop();
-            }
-            rightStack.push(i);
-        }        
-        int maxScore = 0;
-        for (int i = 0; i < n; i++) {
-            if (leftBoundary[i] < k && rightBoundary[i] > k) {
-                int subarrayScore = nums[i] * (rightBoundary[i] - leftBoundary[i] - 1);
-                maxScore = max(maxScore, subarrayScore);
-            }
-        }        
-        return maxScore; 
+        else if(j==n-1)i--;
+      else  if(nums[i-1] < nums[j+1]){
+            j++;
+        }
+        else{
+            i--;
+        }
+        mini = min(mini , min(nums[j],nums[i]));
+        maxi = max(maxi  , (j-i+1)*mini);
+        }
+        return maxi;
     }
 };
