c40326d11befe9e754cfbcdf7b4422547c7ac078
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-06-12 07:31:28 +0530

    Create findxorsumofallpairsbitwiseand.cpp

---
+++ Leetcode Programs/findxorsumofallpairsbitwiseand.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int getXORSum(vector<int>& arr1, vector<int>& arr2) {
+        int xor1 = 0, xor2 = 0;
+        for (int num : arr1) xor1 ^= num;
+        for (int num : arr2) xor2 ^= num;
+        return xor1 & xor2;
+    }
+};
