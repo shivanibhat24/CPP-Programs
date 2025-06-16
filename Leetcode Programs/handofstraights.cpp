8003bdf7b2e582374642907bb7857b9519e54992
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-06-16 11:30:29 +0530

    Create handofstraights.cpp

---
+++ Leetcode Programs/handofstraights.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize)
+    { int n=hand.size();
+      if (n%groupSize!=0) return false;
+      sort(hand.begin(),hand.end());
+      for(int i=0;i<n;i++)
+      {
+        if(hand[i]>=0)
+        {
+            if(!func(hand,groupSize,i,n)) return false;
+        }
+      }
+        return true;
+    }
+    bool func(vector<int>& hand,int groupSize,int i,int n)
+    {
+        int next=hand[i]+1;
+        hand[i]=-1;
+        int count=1;
+        i++;
+        while(i<n && count<groupSize)
+        {
+            if(hand[i]==next)
+            {
+                next=hand[i]+1;
+                hand[i]=-1;
+                count++;
+            }
+            i++;
+        }
+        return count==groupSize;
+    }
+};
