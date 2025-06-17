dc63450079451092960a9ac716e50f1c19d4d670
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-06-17 15:59:06 +0530

    Update nthdigit.cpp

--- Leetcode Programs/nthdigit.cpp
+++ Leetcode Programs/nthdigit.cpp
@@ -1,46 +1,29 @@
 class Solution {
 public:
-    int findNthDigit(long n) {
-        long d=1;
-        long ten=10;
-        long numberd=1;
-        for(int i=1;i<=n;i++){
-            if(i==ten){
-                ten*=10;
-                numberd++;
-            }
-            if(d==n){
-                long num=i;
-                long checkten=ten;
-                long t=d+numberd;
-                while(i>0){
-                    if(d==n){
-                        return num/(checkten/10);
-                    }
-                    d++;
-                    num=num%checkten;
-                    i/=10;
-                    checkten/=10;
-                }                            
-            }
-            if(n>d && n<(d+numberd)){
-                
-                long num=i;
-                long checkten=ten;
-                long t=d+numberd;
-                while(i>0){
-                    if(d==n){
-                        return num/(checkten/10);
-                    }
-                    d++;
-                    num=num%(checkten/10);
-                    i/=10;
-                    checkten/=10;
-                }
-                
-            }
-            d+=numberd;
+    int findNthDigit(int n) {
+        if(n <= 9) return n;
+        if(n == 10) return 1;
+        long long i = 1;
+        long long m = 9;
+        long long x = i * m * pow(10LL, i - 1); 
+        long long digits = 0;
+        while(n >= x) {
+            n -= x;
+            digits = digits * 10 + 9;
+            i++;
+            x = i * m * pow(10LL, i - 1);
         }
-        return -1;
+        long long div = n / i;
+        long long rem = n % i;
+        long long numbersize = i;
+        long long ans;
+        if(rem == 0) return (digits + div) % 10;
+        long long temp = digits + div + 1;
+        while(numbersize >= rem) {
+            ans = temp % 10;
+            temp /= 10;
+            numbersize--;
+        }
+        return (int)ans;
     }
 };
