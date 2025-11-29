ce4b06dae06449ca08f030ca90892ec6051d4b67
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-11-29 12:57:07 +0530

    Create shufflethearray.cpp

---
+++ Leetcode Programs Part 2/shufflethearray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int>ans;
+        int i=0;
+        while(n<nums.size()){
+           ans.push_back(nums[i++]);
+           ans.push_back(nums[n++]);
+        }
+        return ans;
+    }
+};
