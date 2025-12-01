97f0a0b5e758a1ac3dcda4e9a9d553c1efd0b798
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-12-01 15:17:16 +0530

    Create removelinkedlistelements.cpp

---
+++ Leetcode Programs Part 2/removelinkedlistelements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* ans = new ListNode(0, head);
+        ListNode* dummy = ans;
+        while (dummy != nullptr) {
+            while (dummy->next != nullptr && dummy->next->val == val) {
+                dummy->next = dummy->next->next;
+            }
+            dummy = dummy->next;
+        }        
+        ListNode* result = ans->next;
+        delete ans;
+        return result;        
+    }
+};
